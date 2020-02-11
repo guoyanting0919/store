0c3a750f3bdc3abe0ec028e52347cc427c765f3c
YT <guoyanting0919@gmail.com>
2020-02-11 21:30:26 +0800

    about

--- src/views/About.vue
+++ src/views/About.vue
@@ -23,41 +23,137 @@
         <div class="timerImgBox"></div>
       </div>
       <div class="timerTxt">
-        <h2 class="timerTitle">- Since 1998 -</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita voluptas illo blanditiis sequi, harum ipsam nulla dolorum nam eit consectetur adipisicing elit. Expedita voluptas illo blanditiis sequi, harum ipsam nulla dolorum nam eius dolt consectetur adipisicing elit. Expedita voluptas illo blanditiis sequi, harum ipsam nulla dlla dolorum namommos illo blands cs illo blanditiis sequi, har dolorum nam eit consectetur adipisicing elit. Expedita voluptas illo blanditiis sequi, harum ipsam nulla dolorum nam eius dolt consectetur adipisicing elit. Expedita voluptas illo blanditiis sequi, harum ipsam nulla dlla dolorum namommos illo blands cs illo blanditiis sequi, harum ipsam nulla dolorum nams illblanditiis sequi, harum ipsam nulla dolorum namommos illo blands sequi, harum ipsam nulolorum nam eius dolores sapiente ea labore cum perspiciatis cs illo blanditiis sequi, harum ipsam nulla dolorum nams illblanditiis sequi, harum ipsam nulla dolorum namommos illo blands cs illo blanditiis sequi, harum ipsam nulla dolorum nams illblanditiis sequi, harum ipsam nulla dolorum namommos illo blands sequi, harum ipsam nulla dolorum nams illo blanditiis sequi, harum ipsam nulla dolorum namdi architecto? Animi, minus dolorem?</p>
+        <h2 class="timerTitle">- Since 1952 -</h2>
+        <p>
+          雖然台灣的秋冬氣溫比起其他地方相對比較不寒冷，有時厚重的大衣、外套等可能也派不上用場，但如果想穿搭出理想的季節溫度，同時平衡舒適自在的衣著感受的話，推薦大家以騎士皮衣外套加乘，透過萬年經典的款式為日常演繹多變帥氣個性，想知道有哪些搭配方式嗎？一起來看看吧。
+          <br />
+          <span>1952~1960</span>
+          <br />充滿個性的騎士皮衣外套，向來是時尚圈永不退流行的必備單品，當想將帥氣與休閒完美混合時，選搭一件灰、黑色素 TEE 與九分長褲，便能輕鬆連結一股簡約低調的品味特色。
+          <br />
+          <span>1960~1978</span>
+          <br />帶點無拘束風格的連帽上衣，不僅街頭時尚人士經常穿搭，更是懶人最愛的百搭實穿款，選擇連帽上衣外搭騎士皮衣外套，再將帽子拉出於外套上，立即為整體層次營造豐富又加分印象。
+          <br />
+          <span>1978~1999</span>
+          <br />冬季氣溫不穩定有時面對低溫來襲，推薦將上衣換成針織毛衣可以提升造型溫度之外，儘管穿搭騎士皮衣外套，也不會覺得整體太過於保暖，下半身搭配深色或刷破牛仔褲就很好看加分。
+          <br />
+          <span>1999~2014</span>
+          <br />充滿個性的騎士皮衣外套，向但如果想穿搭出理想的季節溫度，同時平衡舒適自在的衣著感受的話，推薦大家以騎士皮衣外套加乘，透過萬年經典的款式為日常演繹多變來是時尚圈永不退流行的必備單品，當想將帥氣與休閒完美混合時，選搭一件灰、黑色素 TEE 與九分長褲，便能輕鬆連結一股簡約低調的品味特色。
+          <br />
+          <span>2014~</span>
+          <br />充滿個性的騎士皮衣外套，向來是時尚圈永不退流行的必備單品，當想將帥氣與休閒完美混合時，選搭一件灰、黑色素 TEE 與九分長褲，便能輕鬆連結一股簡約低調的，選搭一件灰、黑色素 TEE 與九分長褲，便能輕鬆連結一股簡約低調的品味特色。
+        </p>
       </div>
     </div>
 
+    <!-- team -->
+    <div class="team">
+      <div class="teamTxt">
+        <h2 class="teamTitle">- Our Team -</h2>
+        <p>
+          雖然台灣的秋冬氣溫比起其他地方相對比較不寒冷，有時厚重的大衣、外套等可能也派不上用場，但如果想穿搭出理想的季節溫度，同時平衡舒適自在的衣著感受的話，推薦大家以騎士皮衣外套加乘，透過萬年經典的款式為日常演繹多變帥氣個性，想知道有哪些搭配方式嗎？一起來看看吧。
+          <span>店長-星辰</span>充滿個性的騎士皮衣外套，向來是時尚圈永不退流行的必備單品，當想將帥氣與休閒完美混合時，選搭一件灰、黑色素 TEE 與九分長褲，便能輕鬆連結一股簡約低調的品味特色。
+          <span>副店長-阿嘉</span>>帶點無拘束風格的連帽上衣，不僅街頭時尚人士經常穿搭，更是懶人最愛的百搭實穿款，選擇連帽上衣外搭騎士皮衣外套，再將帽子拉出於外套上，立即為整體層次營造豐富又加分印象。
+          <span>採購長-國棟</span>
+          冬季氣溫不穩定有時面對低溫來襲，推薦將上衣換成針織毛衣可以提升造型溫度之外，儘管穿搭騎士皮衣外套，也不會覺得整體太過於保暖，下半身搭配深色或刷破牛仔褲就很好看加分。
+          <span>採購長-Shan</span>
+          充滿個性的騎士皮衣外套，向但如果想穿搭出理想的季節溫度，同時平衡舒適自在的衣著感受的話，推薦大家以騎士皮衣外套加乘，透過萬年經典的款式為日常演繹多變來是時尚圈永不退流行的必備單品，當想將帥氣與休閒完美混合時，選搭一件灰、黑色素 TEE 與九分長褲，便能輕鬆連結一股簡約低調的品味特色。
+          <span>優質店員-小慧</span>
+          充滿個性的騎士皮衣外套，向來是時尚圈永不退流行的必備單品，當想將帥氣與休閒完美混合時，選搭一件灰、黑色素 TEE 與九分長褲，便能輕鬆連結一股簡約低調的，選搭一件灰、黑色素 TEE 與九分長褲，便能輕鬆連結一股簡約低調的品味特色。
+          <span>優質店員-Jocilen</span>
+          充滿個性的騎士皮衣外套，向來是時尚圈永不退流行的必備單品，當想將帥氣與休閒完美混合時，選搭一件灰、黑色素 TEE 與九分長褲，便能輕鬆連結一股簡約低調的，選搭一件灰、黑色素 TEE 與九分長褲，便能輕鬆連結一股簡約低調的品味特色。
+        </p>
+      </div>
+      <div class="teamImgPadding">
+        <div class="teamImgBox"></div>
+      </div>
+    </div>
     <!-- slogan -->
     <div class="slogan">
       <div class="sloganLine"></div>
       <h5>「風格，才是個人穿搭最重要的事情。」</h5>
     </div>
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
+
+    <!-- aboutStore -->
+    <div class="aboutStore">
+      <div class="container aboutStoreContainer">
+        <div class="row h-100">
+          <div class="col-12 col-lg-5 responsiveH100vh">
+            <div class="row h-100 aboutBg aboutBgR">
+              <div class="col-8 aboutStoreImg aboutStoreImg1"></div>
+              <div class="col-4">
+                <div class="row detailMt">
+                  <div class="col-12">
+                    <div class="detailImg1 detailImg"></div>
+                  </div>
+                  <div class="col-12">
+                    <div class="detailImg2 detailImg"></div>
+                  </div>
+                  <div class="col-12">
+                    <div class="detailImg3 detailImg"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-lg-2 aboutSlogan my-4">
+            <span>Personal</span>
+            <span>&</span>
+            <span>Style</span>
+          </div>
+          <div class="col-12 col-lg-5 responsiveH100vh">
+            <div class="row h-100 aboutBg aboutBgB">
+              <div class="col-4">
+                <div class="row detailMt">
+                  <div class="col-12 aboutFlexRow">
+                    <div class="detailImg4 detailImg"></div>
+                  </div>
+                  <div class="col-12 aboutFlexRow">
+                    <div class="detailImg5 detailImg"></div>
+                  </div>
+                  <div class="col-12 aboutFlexRow">
+                    <div class="detailImg6 detailImg"></div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-8 aboutStoreImg aboutStoreImg2"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- aboutFooter -->
+    <div class="aboutFooterImg mt-5">
+      <div class="footerImg">
+        <div class="footerContainerBg">
+          <div class="container-fluid">
+            <div class="row h-100">
+              <div class="col-12 col-lg-4 footerIcon">
+                <i class="fab fa-facebook-square leftIcon"></i>
+                <i class="fab fa-instagram leftIcon"></i>
+                <i class="fab fa-line leftIcon"></i>
+                <i class="fab fa-twitter-square leftIcon"></i>
+              </div>
+              <div class="col-12 col-lg-4 footerInfo">
+                <p>
+                  <i class="fas fa-map-marker-alt"></i>地址:桃園市蘆竹區福祿一街189號
+                </p>
+                <p>
+                  <i class="fas fa-phone"></i>電話:03-3491223
+                </p>
+                <p>
+                  <i class="far fa-envelope"></i>E-mail:logo@gmail.com
+                </p>
+              </div>
+              <div class="col-12 col-lg-4 footerCopy">
+                <p>個人練習作品，非商業用途。</p>
+                <i class="fab fa-github"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -81,7 +177,7 @@ export default {
           var top = $(element).offset().top;
           var height = $(element).height();
           var bottom = top + height / 2;
-          if (scrollBottom >= bottom && scrollTop < bottom) {
+          if (scrollBottom >= bottom) {
             $(element).css("opacity", 1);
             $(element).css("transform", "translateY(0px)");
             $(element).css("transition", duration / 1000 + "s");
@@ -95,8 +191,9 @@ export default {
     };
     Pb.appear(".slogan", 1500);
     Pb.appear(".timerTitle", 1500);
+    Pb.appear(".teamTitle", 1500);
     Pb.opacityZero(".slogan");
-    Pb.opacityZero(".timerTitle");
+    Pb.opacityZero(".teamTitle");
   }
 };
 </script>
